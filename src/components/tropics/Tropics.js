3c11c4bf443ffb059e6ddcbde3b72548349156d1
abusayed0 <abusayed48876@gmail.com>
2022-12-18 04:58:24 +0600

    added top banner

--- src/components/tropics/Tropics.js
+++ src/components/tropics/Tropics.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Tropics = () => {
     return (
-        <div>
-            <h2>This is Tropics Page !</h2>
+        <div className="mt-10 md:w-10/12 md:mx-auto relative">
+            <div className="h-80vh">
+               <img src="https://media.istockphoto.com/id/1398473177/photo/questionnaire-with-checkboxes-filling-survey-form-online-answer-questions.jpg?b=1&s=170667a&w=0&k=20&c=Ld-E8ElmNgDKGxFBVyVFqqBDG6byHF98amsRMYFEedk=" alt="" className="w-full h-full brightness-50" />
+            </div>
+            <div className="absolute top-0 h-full w-full flex justify-center flex-col p-5 md:p-10 gap-4 md:gap-6">
+                <h2 className="text-2xl font-bold text-white">Know Your Present Knowledge Level With Pro Quizzer !</h2>
+                <p className="text-white text-xl">Wellcome to Pro Quizzer. Pro Quizzer is one the most popular online based quiz contest organaizer.we have the best team and our team members are very much serious to provide quality question.let test yourself. </p>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      height:{
+          "80vh":"80vh"
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
